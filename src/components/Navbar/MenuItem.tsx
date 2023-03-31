3a7d5ad87b263faaecae429d319bab1689c89549
Alexandre <alexandre01393@gmail.com>
2023-03-31 17:43:22 -0300

    feat: navbar UI

---
+++ src/components/Navbar/MenuItem.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+interface MenuItemProps {
+  onClick: () => void;
+  label: string;
+}
+
+const MenuItem = ({ label, onClick }: MenuItemProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+    >
+      {label}
+    </button>
+  );
+};
+
+export default MenuItem;
---
+++ src/components/Navbar/UserMenu.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
+
+const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  return (
+    <div className="relative ">
+      <div className="flex items-center gap-3">
+        <button
+          onClick={() => {
+            console.log('oi');
+          }}
+          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition"
+        >
+          Airbnb your home
+        </button>
+        <button
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex items-center gap-3 rounded-full hover:shadow-md transition"
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
+        </button>
+      </div>
+      {isOpen && (
+        <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
+          <button className="flex flex-col">
+            <>
+              <MenuItem
+                label="Login"
+                onClick={() => {
+                  console.log('oi');
+                }}
+              />
+              <MenuItem label="Sign up" onClick={() => console.log('oi')} />
+            </>
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
